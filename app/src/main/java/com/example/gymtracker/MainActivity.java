14248966590faf091ccfca8cac0e8d9e0a660a95
SylwesterS <sylancompany@gmail.com>
2021-02-25 17:16:44 +0000

    Testing Layouts

--- app/src/main/java/com/example/gymtracker/MainActivity.java
+++ app/src/main/java/com/example/gymtracker/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.gymtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.widgets.ConstraintHorizontalLayout;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @SuppressLint("SetTextI18n")
+    public void changeName(View v) {
+        //R.id.textView
+        TextView textViewToChange = (TextView) findViewById(R.id.textView);
+        EditText text = (EditText) findViewById(R.id.editTextTextPersonName2);
+        //textViewToChange.setText(
+        //        "" + text.getText());
+        TextView mycomponent;
+        mycomponent = new TextView(getApplicationContext());
+        mycomponent.setText(text.getText());
+
+        ConstraintLayout conL = (ConstraintLayout) findViewById(R.id.conLay);
+
+        mycomponent.setLayoutParams(textViewToChange.getLayoutParams());
+        mycomponent.setY(mycomponent.getY() - 50);
+        conL.addView(mycomponent);
     }
 }
