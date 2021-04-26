64893029793ad3f0aa3c0cef3e3e68dccdc5ee2e
SylwesterS <sylancompany@gmail.com>
2021-04-27 00:46:06 +0100

    Notes, planning

---
+++ app/src/main/java/com/example/gymtracker/WorkTask.java
@@ -0,0 +1,13 @@
+package com.example.gymtracker;
+
+public class WorkTask {
+    /*
+    This will be a data structure which will hold our work task.
+    Work task is something that users will be able to create to add to their todo list.
+    Work components:
+    name
+    description
+    date
+    type
+     */
+}
